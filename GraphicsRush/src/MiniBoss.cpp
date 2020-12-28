4bd9fc2db6e9b893cf2e8db7fc9befcb990483aa
William Chao <william09172000@gmail.com>
2020-12-28 20:39:55 +0800

    tried to write shader for miniboss but failed

--- GraphicsRush/src/MiniBoss.cpp
+++ GraphicsRush/src/MiniBoss.cpp
@@ -1,5 +1,5 @@
 #include "MiniBoss.H"
 float MiniBoss::clipping = -99;
-float MiniBoss::bossLane = 2;
+float MiniBoss::bossLane = 5;
 float MiniBoss::bossHeight = 10;
 float MiniBoss::bossTarget = 0;
--- GraphicsRush/src/TrainView.cpp
+++ GraphicsRush/src/TrainView.cpp
@@ -1149,6 +1149,7 @@ void TrainView::drawMiniBoss() {
 	miniBossCross = normalize(miniBossCross);
 	miniBossPos += miniBossUp * MiniBoss::bossHeight;
 	miniBossPos += miniBossCross * MiniBoss::bossLane * 10.0f;
+
 	glBegin(GL_QUADS);
 	glColor3f(1.0f, 0.0f, 0.0f);
 	glVertex3f(miniBossPos.x + miniBossUp.x, miniBossPos.y + miniBossUp.y, miniBossPos.z + miniBossUp.z);
@@ -1374,10 +1375,12 @@ draw()
 
 		if (m_pTrack->miniBoss) {
 			if (clock() - clipTime > CLOCKS_PER_SEC * 3) {
-				MiniBoss::bossTarget = (rand() % 3) - 1;
+				int new_target = (rand() % 3) - 1;
+				while (new_target == MiniBoss::bossTarget) new_target = (rand() % 3) - 1;
+				MiniBoss::bossTarget = new_target;
 				clipTime = clock();
 			}
-			if (abs(MiniBoss::bossLane - MiniBoss::bossTarget) < 0.02) MiniBoss::clipping = MiniBoss::bossTarget;
+			if (abs(MiniBoss::bossLane - MiniBoss::bossTarget) < 0.1) MiniBoss::clipping = MiniBoss::bossTarget;
 			else MiniBoss::clipping = -99;
 		}
 
